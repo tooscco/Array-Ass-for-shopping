4fa381f385ff33ead398586c3a31d473dc6b26de
tooscco <aladejuyigbetosin7@gmail.com>
2024-03-20 01:30:54 +0000

    still working

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var cart = []
 
+if (cart.length == 0){
+    notifyDiv.style.display = 'block'
+    notifyDiv.innerHTML = '<p class="text-center bg-dark-subtle p-3 mx-4">No items added yet</>'
+}
+
 function addItem() {
     if (items.value ===''){
         // alert('Fill in the empty spaces')
-        err.innerHTML =`Fill in the empty spaces`
+        err.style.display= 'block'
     } else {
+        notifyDiv.style.display = 'none'
+        err.style.display= 'none'
+        divBtn.style.display = 'block'
         cart.push(items.value)
         document.getElementById('items').value = ''
         displayItem()
@@ -23,8 +31,29 @@ function deleteLast(){
     displayItem()
 }
 
-function addStart(){
+function addFirst(){
     cart.unshift(items.value)
+    displayItem()
+}
+
+function delAll(){
+    var confirmation = confirm('Are you sure you want to delete. This action is irreversible')
+    if (confirmation == true){
+        cart.splice(0, cart.length)
+        displayItem()
+    }
+}
+
+function editItem() {
+    var ediAny = Number(prompt('which number are you editing'))
+    var replay = prompt('new value')
+    cart.splice(ediAny-1, 1, replay)
+    displayItem()
+}
+
+function deleteAny(){
+    var any = Number(prompt('Which number are you deleting'))
+    cart.splice(any-1, 1)
     displayItem()
 }
 
